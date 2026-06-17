dd9724dc95c370dd7cd2dacd6b9d1555701b5565
Zain Ul Rauf <zainulrauf@gmail.com>
2026-06-17 12:01:54 +0500

    add link

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -3,5 +3,5 @@ import axios from "axios";
 export const api = axios.create({
   baseURL:
     process.env.NEXT_PUBLIC_API_URL ||
-    "http://localhost:3001",
+    "https://tax-backend-ff5k.vercel.app",
 });
